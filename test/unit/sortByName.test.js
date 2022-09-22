e15e61f49badccd0c6bc37c4f0d4326560f86c48
Stjushenka <stjushenka@gmail.com>
2022-09-22 18:49:11 +0300

    add

---
+++ test/unit/sortByName.test.js
@@ -0,0 +1,30 @@
+const sorting = require("../../app");
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order", () => {
+    expect(
+      sorting.sortByName([
+        "Гарри Поттер",
+        "Властелин Колец",
+        "Волшебник изумрудного города",
+      ])
+    ).toEqual([
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]);
+  });
+  it('Meds names should be sorted in ascending order', () => {
+    expect(
+        sorting.sortByName([
+            'Понедельник начинается в субботу',
+            'Мартин Иден',
+            'Понедельник начинается в субботу',
+        ]),
+    ).toEqual([
+        'Мартин Иден',
+        'Понедельник начинается в субботу',
+        'Понедельник начинается в субботу',
+    ]);
+});
+});
